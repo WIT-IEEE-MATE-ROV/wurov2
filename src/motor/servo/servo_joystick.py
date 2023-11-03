3adf0742e23edf6c0324835b952951bde75f365a
Tedi Qafko <tedi.qafko@gmail.com>
2023-11-02 22:47:37 -0400

    Servo control from joystick

--- src/motor/servo/servo_joystick.py
+++ src/motor/servo/servo_joystick.py
@@ -1,51 +1,57 @@
-import time
+#!/usr/bin/env python3
 
+import rospy
+from std_msgs.msg import UInt16
+from geometry_msgs.msg import Vector3
+import time
 from board import SCL, SDA
 import busio
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 
 i2c = busio.I2C(SCL, SDA)
-
-# Create a simple PCA9685 class instance.
 pca = PCA9685(i2c)
-
-# pca = PCA9685(i2c, reference_clock_speed=25630710)
 pca.frequency = 50
 
-# To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
-# match the stall points of the servo.
-# This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
-# servo7 = servo.Servo(pca.channels[7], min_pulse=580, max_pulse=2350)
-# This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
-#   https://www.adafruit.com/product/2307
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2600)
-# This is an example for the Standard servo - TowerPro SG-5010 - 5010:
-#   https://www.adafruit.com/product/155
-# servo7 = servo.Servo(pca.channels[7], min_pulse=400, max_pulse=2400)
-# This is an example for the Analog Feedback Servo: https://www.adafruit.com/product/1404
-# servo7 = servo.Servo(pca.channels[7], min_pulse=600, max_pulse=2500)
-# This is an example for the Micro servo - TowerPro SG-92R: https://www.adafruit.com/product/169
-# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2400)
+servo0 = servo.Servo(pca.channels[0],min_pulse=1100, max_pulse=1900)
+servo1 = servo.Servo(pca.channels[1],min_pulse=1100, max_pulse=1900)
 
-# The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
-# range, but the default is to use 180 degrees. You can specify the expected range if you wish:
-# servo7 = servo.Servo(pca.channels[7], actuation_range=135)
-servo0 = servo.Servo(pca.channels[0])
+# # We sleep in the loops to give the servo time to move into position.
+# for i in range(180):
+#     servo0.angle = i
+#     time.sleep(0.03)
+# for i in range(180):
+#     servo0.angle = 180 - i
+#     time.sleep(0.03)
 
-# We sleep in the loops to give the servo time to move into position.
-for i in range(180):
-    servo7.angle = i
-    time.sleep(0.03)
-for i in range(180):
-    servo7.angle = 180 - i
-    time.sleep(0.03)
+# # You can also specify the movement fractionally.
+# fraction = 0.0
+# while fraction < 1.0:
+#     servo0.fraction = fraction
+#     fraction += 0.01
+#     time.sleep(0.03)
 
-# You can also specify the movement fractionally.
-fraction = 0.0
-while fraction < 1.0:
-    servo7.fraction = fraction
-    fraction += 0.01
-    time.sleep(0.03)
+# pca.deinit()
 
-pca.deinit()
+def map_range(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+def callback(data):
+    x = map_range(data.x,-1, 1, 0, 180)
+    y = map_range(data.y,-1, 1, 0, 180)
+    
+    servo0.angle = x
+    servo1.angle = y
+    
+    
+    
+def servo_joystick():
+    # Initilizes a default ros node 
+    rospy.init_node('servoControl', anonymous=True)
+    sub = rospy.Subscriber("pca_linearMovement", Vector3, callback) # data recieved calls function callback()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    servo_joystick()
