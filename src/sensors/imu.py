449b3c9e3889d13b0448993cbf5b2b2e6cc67c8d
jetson0 <jetson0@nugget.com>
2023-12-01 04:58:20 +0100

    BNO085 test code

--- src/sensors/imu.py
+++ src/sensors/imu.py
@@ -33,3 +33,18 @@ while True:
     print("Gyroscope (radians/s): ({0:0.3f},  {1:0.3f},  {2:0.3f})".format(gyro_x, gyro_y, gyro_z))
 
     time.sleep(0.1)
+
+
+
+# import board
+# import busio
+# from adafruit_bno08x.i2c import BNO08X_I2C
+# from adafruit_bno08x import BNO_REPORT_ACCELEROMETER
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+# bno = BNO08X_I2C(i2c)
+# bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+
+# while True:
+#     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
+#     print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
